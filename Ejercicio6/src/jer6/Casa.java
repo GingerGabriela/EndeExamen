eb7f519c45b1eec9768fdb4c90ba38e89d20eed4
Ginger <gnortiz25@gmail.com>
2019-05-24 13:29:35 +0200

    cambios en limpiarCasa

---
+++ Ejercicio6/src/jer6/Casa.java
@@ -0,0 +1,20 @@
+package jer6;
+
+public class Casa {
+
+
+		private float superficie;
+		private String calle;
+		private int codigoPosta;
+		
+		/**
+		 * Un equipo de limpiadores deja la casa libre de gérmenes 
+		 * y desinfecta todas las superficies
+		 */
+		
+		public void limpiarLaCasa() {
+			String mensajito="Te hemos limpiado la casa,estás arruinado ";
+			System.out.println(mensajito);
+		}
+	
+}
